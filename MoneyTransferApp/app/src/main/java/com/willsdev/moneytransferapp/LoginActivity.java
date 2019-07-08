17fba62f4a3938984449f7026e81205da63c39be
Michael Fitzmaurice <michael@ezzydoc.com>
2019-07-08 10:28:03 +0800

    Added database connection

--- MoneyTransferApp/app/src/main/java/com/willsdev/moneytransferapp/LoginActivity.java
+++ MoneyTransferApp/app/src/main/java/com/willsdev/moneytransferapp/LoginActivity.java
@@ -1,48 +1,98 @@
 package com.willsdev.moneytransferapp;
 
-import android.content.Context;
+import android.app.Application;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-class NetworkThread extends AsyncTask<String, Void, String>
+class RunQuery {
+    DBController dbController;
+    QueryType queryType;
+    Map<String, String> data;
+
+    public RunQuery(DBController dbController, QueryType queryType, Map<String,String> data) {
+        this.dbController = dbController;
+        this.queryType = queryType;
+        this.data = data;
+    }
+}
+
+enum QueryType {
+    LOGIN,GET_WALLETS,TRANSFER,SIGNUP
+}
+
+/**
+ * <Do in background,
+ */
+class NetworkThread extends AsyncTask<RunQuery, String, Map>
 {
-    private Context context;
+    private Application application;
+    private RunQuery runQuery;
 
-    public NetworkThread(Context context){
-        this.context=context;
+    NetworkThread(Application application, RunQuery runQuery){
+        this.application=application;
+        this.runQuery=runQuery;
     }
 
-    protected String doInBackground(String... arg0) {
-        String host = "jdbc:mysql://91.132.103.123:3306/app?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-        String user = "root";
-        String pass = "123123123App";
-        DBController controller = new DBController(host, user, pass);
+    protected Map doInBackground(RunQuery... arg0) {
         Map map = null;
-        try
-        {
-            map = controller.getData();
-        } catch (Exception e)
-        {
-            e.printStackTrace();
+        if(runQuery.dbController==null) {
+            String host = "jdbc:mysql://91.132.103.123:3306/app?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            String user = "root";
+            String pass = "123123123App";
+            runQuery.dbController = new DBController(host,user,pass);
+            ((MyApplication)application).dbController = runQuery.dbController;
         }
+        switch (runQuery.queryType) {
+            case LOGIN:{
+                String login_query = "select * from users where login=\""+runQuery.data.get("user")+"\" and pass=\"" + runQuery.data.get("pass") + "\"";
+                try
+                {
+                    int result = runQuery.dbController.get_count(login_query);
+                    if(result>0){
+                        Intent intent = new Intent(application.getApplicationContext(), MainActivity.class);
+                        application.getApplicationContext().startActivity(intent);
+                    }
+                } catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            case GET_WALLETS:{
+                String user_id = runQuery.data.get("user_id");
+                String query = "select * from accounts where users_user_id="+user_id;
+                try {
+                    //map = runQuery.dbController.getData(query);
+                } catch (Exception e){}
+            }
+            case TRANSFER:{
+                String user_id = runQuery.data.get("user_id");
+                String initial_amt = runQuery.data.get("initial_amt");
+                String final_amt = runQuery.data.get("final_amt");
+                String query = "update";
 
-        Toast.makeText(context, map.toString(), Toast.LENGTH_LONG).show();
-
-        return null;
-    }
-
-    protected void onPostExecute(String result) {
-        Intent intent = new Intent(context, MainActivity.class);
-        context.startActivity(intent);
+            }
+            case SIGNUP:{
+                Map<String, String> data = runQuery.data;
+                String user = data.get("user");
+                String pass = data.get("pass");
+                String name = data.get("name");
+                String country = data.get("country");
+                String language = data.get("language");
+                String query = "";
+            }
+        }
+        return map;
     }
 }
 
@@ -55,16 +105,23 @@ public class LoginActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText user = findViewById(R.id.login_user);
-        EditText pass = findViewById(R.id.login_pass);
+        getApplication();
 
-        Button login = findViewById(R.id.btn_login);
-        login.setOnClickListener(new View.OnClickListener()
+        String login = ((EditText)findViewById(R.id.login_user)).getText().toString();
+        String pass = ((EditText)findViewById(R.id.login_pass)).getText().toString();
+        Map<String,String> data = new HashMap<>();
+        data.put("user",login);
+        data.put("pass",pass);
+        final RunQuery runQuery = new RunQuery(null, QueryType.LOGIN, data);
+
+        Button login_btn = findViewById(R.id.btn_login);
+        login_btn.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                new NetworkThread(getApplicationContext()).execute("Your URL here");
+                NetworkThread networkThread = new NetworkThread(getApplication(), runQuery);
+                networkThread.execute(runQuery);
             }
         });
 
@@ -78,10 +135,5 @@ public class LoginActivity extends AppCompatActivity
                 startActivity(intent);
             }
         });
-    }
-
-    public void gotoBlah() {
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
     }
 }
